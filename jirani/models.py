93d57495f737543e579ccfe03257d4dc59353d1d
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-30 09:54:23 +0300

    created first table and its migrations

--- jirani/models.py
+++ jirani/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Location(models.Model):
+    locations = models.CharField(max_length=65)
+       
+    name = models.CharField(max_length=65)
+
+
+
+    def save_loc(self):
+        self.save()
+
+    def delete_loc(self):
+        self.delete()
+
+
+    def __str__(self):
+        return self.name
