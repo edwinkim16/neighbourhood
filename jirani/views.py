38cdc31af68bb4c3af8a184425f26092e2358320
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-30 12:12:52 +0300

    creating home view and its url

--- jirani/views.py
+++ jirani/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http  import HttpResponse
+from .models import Hood, Profile, Business, Post,Location
 
 # Create your views here.
 
 def welcome(request):
     return render(request, 'welcome.html')
+
+def home(request):
+    hoods = Hood.objects.all()
+    return render(request,'home.html',locals())    
