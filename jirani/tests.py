353ed5cb04a2f1a5aec3db09cbc401e26c463c92
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-30 10:42:50 +0300

    testing location methods

--- jirani/tests.py
+++ jirani/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Hood, Profile, Business, Post,Location
+from django.contrib.auth.models import User
 
 # Create your tests here.
+
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.tao =Location(name='tao',locations='survey')
+        
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.tao,Location))    
+
+    def test_save_loc(self):
+        self.tao.save_loc()
+        looc = Location.objects.all()
+        self.assertTrue(len(Location)>0)    
+
