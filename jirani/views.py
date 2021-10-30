f3cf08a5e31d833afe979d6abfc63b75778cbaa3
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-30 09:26:31 +0300

    registering templates

--- jirani/views.py
+++ jirani/views.py
@@ -4,4 +4,4 @@ from django.http  import HttpResponse
 # Create your views here.
 
 def welcome(request):
-    return HttpResponse('Welcome to hood watch')
+    return render(request, 'welcome.html')
