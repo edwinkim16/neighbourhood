ae323a1981834f5dcc78612dc2e3da3d100cfe08
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-30 10:00:09 +0300

    created hood table

--- jirani/models.py
+++ jirani/models.py
@@ -25,4 +25,11 @@ class Category(models.Model):
     name = models.CharField(max_length=30)
 
     def __str__(self):
-        return self.name        
+        return self.name    
+        
+class Hood(models.Model):
+    name = models.CharField(max_length=50)
+    image = CloudinaryField('image')
+    occupants = models.CharField(max_length=50)
+    location = models.ForeignKey(Location,on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True, null=True)            
