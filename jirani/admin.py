296ca49d9966bc9185133fe16b2817d72e4d53b1
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-30 11:18:15 +0300

    registering models to admin

--- jirani/admin.py
+++ jirani/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Hood, Profile, Business, Post,Location,Category
 
 # Register your models here.
+admin.site.register(Hood)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Post)
