0a1c7fced1fe717942a054445a5ce6cc77c7da6f
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-30 10:48:36 +0300

    testing hood methods

--- jirani/tests.py
+++ jirani/tests.py
@@ -17,3 +17,18 @@ class LocationTestClass(TestCase):
         looc = Location.objects.all()
         self.assertTrue(len(Location)>0)    
 
+class HoodTestClass(TestCase):
+    def setUp(self):
+        self.tao =Hood(name='tao',location='survey',occupants=5)
+        # self.my_hood.save()
+
+    def tearDown(self):
+        Hood.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.tao,Hood))
+
+    def test_save_hood(self):
+        self.tao.save_hood()
+        hood = Hood.objects.all()
+        self.assertTrue(len(Hood)>0)
