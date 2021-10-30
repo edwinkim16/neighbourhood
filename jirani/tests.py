d57ee8f26fab4b519a389865f19c7cb940188b82
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-30 11:03:22 +0300

    creating business tests

--- jirani/tests.py
+++ jirani/tests.py
@@ -52,4 +52,25 @@ class profileTestCLass(TestCase):
         self.you.save_profile()
         bio = Profile.objects.all()
         self.assertTrue(len(bio) > 0)
-                
+
+class BusinessTestClass(TestCase):
+    def setUp(self):
+        self.new_user=User(username="world",email="bs@gmail.com")
+        self.new_user.save()
+        self.my_hood = Hood(name='tao', location='surveys', occupants=3)
+        self.my_hood.save()
+        self.burgers = Business(business_name='mat biz',email='bs@gmail.com',owner=self.new_user,hood=self.my_hood)
+        self.burgers.save()
+
+    def tearDown(self):
+        User.objects.all().delete()
+        Hood.objects.all().delete()
+        Business.objects.all().delete()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.burgers,Business))
+
+    def test_save_business(self):
+        self.burgers.create_business()
+        business =  Business.objects.all()
+        self.assertTrue(len(business)>0)                    
