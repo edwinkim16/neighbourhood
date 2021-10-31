206c2cfb4865900ed7cba464df8ae13417e5ee88
edwinkim16 <edwin.kithinji@student.moringaschool.com>
2021-10-31 07:40:04 +0300

    adding hood view

--- jirani/urls.py
+++ jirani/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     #url('^$',views.welcome,name = 'welcome'),
     url(r'^$', views.home, name='home'),
+    url(r'^hood/(?P<hood_id>\d+)', views.hood, name='hood'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- jirani/views.py
+++ jirani/views.py
@@ -10,3 +10,12 @@ def welcome(request):
 def home(request):
     hoods = Hood.objects.all()
     return render(request,'home.html',locals())    
+
+def hood(request,hood_id):
+    current_user = request.user
+    hood_name = current_user.profile.hood
+    hood = Hood.objects.get(id=request.user.profile.hood.id)
+    businesses = Business.get_business(hood_id)
+    posts = Post.get_post(hood_id)
+
+    return render(request,'hood.html',locals())    
